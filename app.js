a8cd8d2d1ba046ff3e41ea41b27e598cac72f971
Javad Ghanbari <javadghanbari.put@gmail.com>
2024-02-10 22:15:49 -0800

    Getting the name of the city from the user

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 const BASE_URL = "https://api.openweathermap.org/data/2.5";
 const API_KEY = "dae9eb3d2c2c91dada0e72afd4792469";
 
+const searchInput = document.querySelector("input");
+const searchButton = document.querySelector("button");
+
+const searchHandler = (city) => {
+    const cityName = searchInput.value;
+
+    if (!cityName) {
+        alert("Please enter city name!")
+    }
+}
+
+searchButton.addEventListener("click",searchHandler)
